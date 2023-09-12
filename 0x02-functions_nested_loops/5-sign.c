004eac08018eb7b06b9453ddc2870f341c5b309f
kata0321 <tpgare3@gmail.com>
2023-09-12 16:54:34 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+		_putchar('+');
 		return (1);
-	} else if (n < 0)
+	} else if (n == 0)
 }
-		putchar(45);
-		return (-1);
-	} else
-	{
-		putchar(48);
-	}
+		_putchar(48);
 		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
 }
