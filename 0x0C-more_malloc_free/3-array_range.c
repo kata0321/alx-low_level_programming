5f2412ac223e488fdda08aa8a8ba03bf8145fac3
kata0321 <tpgare3@gmail.com>
2023-10-08 06:04:01 -0700

    task3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- **array_range
+ **array_range - creates an array of integers
  * @min: minimum range of values stored
  * @max: maximum range of values stored and number of elements
  *
