d8886a5b2e49b644d80ace766390302d43718591
kata0321 <tpgare3@gmail.com>
2023-10-26 13:24:24 -0700

    task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,39 +1,28 @@
 #include "main.h"
 
 /**
- * binary_to_uint - function converts a binary number to an unsigned int.
+ * binary_to_uint - function converts a binary to unsigned
  * @b: pointer to a string containing a binary number
  *
  * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int binary_to_uint(const char *b)
+int a;
+unsigned int num;
+num = 0;
+if (!b)
+return (0);
+for (a = 0; b[a] != '\0'; a++)
 {
-if (b == NULL) {
-return 0;
+if (b[a] != '0' && b[a] != '1')
+return (0);
 }
-unsigned int result = 0;
-for (int i = 0; b[i] != '\0'; i++)
+for (a = 0; b[a] != '\0'; a++)
 {
-char digit = b[i];
-if (digit == '0')
-{
-result <<= 1; 
-} else if (digit == '1')
-{
-result = (result << 1) | 1;
-} else
-{
-return 0;
+num <<= 1;
+if (b[a] == '1')
+num += 1;
 }
-}
-return result;
-}
-int main() 
-{
-const char *binaryStr = "11010";
-unsigned int decimalValue = binary_to_uint(binaryStr);
-printf("Binary: %s\nDecimal: %u\n", binaryStr, decimalValue);
-return 0;
+return (num);
 }
