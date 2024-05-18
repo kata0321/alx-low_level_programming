90c53c4cdaad8d899d2842dccb10201a86f1ff28
kata0321 <tpgare3@gmail.com>
2024-05-18 11:47:51 -0700

    update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,15 +9,11 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a > b && a > c)
+if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
+else if (b >= a && b >= c)
 {
 largest = b;
 }
@@ -25,5 +21,6 @@ else
 {
 largest = c;
 }
+
 return (largest);
 }
